b59ba9b9961a58f464bd4a18e02d564cfaab485d
lxkeepcoding <lx3332551532@163.com>
2021-08-18 23:44:52 +0800

    first

---
+++ 模板初阶.cpp
@@ -0,0 +1,26 @@
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//
+//}
+//void Swap(int& left, int& right)
+//{
+//	int temp = left;
+//	left = right;
+//	right = temp;
+//}
+//void Swap(double& left, double& right)
+//{
+//	double temp = left;
+//	left = right;
+//	right = temp;
+//}
+//void Swap(char& left, char& right)
+//{
+//	char temp = left;
+//	left = right;
+//	right = temp;
+//}
+//......
+
